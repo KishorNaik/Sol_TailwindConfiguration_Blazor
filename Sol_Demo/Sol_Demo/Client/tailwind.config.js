e44b8c90af5b3fb0e395303f6d506eb5fd84e570
Kishor Naik <kishor.naik011.net@gmail.com>
2021-08-21 10:26:02 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Client/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: ["../**/*.razor"],
+    prefix: 'tw-',
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
